319083889580e6ca6d4774d1e7d7aea5a66c82cf
Luke Reynolds <31745084+lukereynoldspi@users.noreply.github.com>
2021-11-14 15:56:02 -0800

    split into two classes

--- bike.rb
+++ bike.rb
@@ -3,27 +3,39 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  attr_accessor :id, :color, :price, :weight, :rented, :bike_cargo
+
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false, bike_cargo = nil)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @bike_cargo = bike_cargo
   end
 
   def rent!
     self.rented = true
   end
 
+end
+
+class BikeCargo
+  
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize
+    @cargo_contents = []
+  end
+
   def add_cargo(item)
     self.cargo_contents << item
   end
-
+  
   def remove_cargo(item)
     self.cargo_contents.remove(item)
   end
@@ -35,5 +47,5 @@ class Bike
   def pannier_remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
-
 end
+  
--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
 !# /usr/bin/env ruby
 
 require_relative 'bike'
+bike_cargo = BikeCargo.new
 
-bike = Bike.new(1, :pink, 99.99)
+bike_cargo.add_cargo(:apples)
+bike_cargo.add_cargo(:water)
+bike_cargo.add_cargo(:repair_kit)
+bike = Bike.new(1, :pink, 99.99, 200, false, bike_cargo)
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
 
-puts "Space for #{bike.pannier_remaining_capacity} items left."
+puts "Space for #{bike_cargo.pannier_remaining_capacity} items left."
 
 bike.rent!
